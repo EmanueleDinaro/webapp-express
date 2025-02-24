c5d9a2aa5ff3e5cdf4bb298b3d1782f0553dbb87
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-24 16:35:31 +0100

    feat: add controllers and routes

---
+++ controllers/moviesControllers.js
@@ -0,0 +1,21 @@
+const connection = require("../data/db");
+
+//Index
+function index(req, res) {}
+
+//Show
+function show(req, res) {}
+
+//Store
+function store(req, res) {}
+
+//Update
+function update(req, res) {}
+
+//Modify
+function modify(req, res) {}
+
+//Destroy
+function destroy(req, res) {}
+
+module.exports = { index, show, store, update, modify, destroy };
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const moviesControllers = require("../controllers/moviesControllers");
+
+//Index
+router.get("/", moviesControllers.index);
+
+//Show
+router.get("/:id", moviesControllers.show);
+
+//Store
+router.post("/", moviesControllers.store);
+
+//Update
+router.pu("/:id", moviesControllers.update);
+
+//Modify
+router.patch("/:id", moviesControllers.modify);
+
+//Destroy
+router.delete("/:id", moviesControllers.destroy);
 
 module.exports = router;
